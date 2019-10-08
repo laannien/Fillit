9dc1490fa343f446a548ee5f9d12ae5368ba82c5
djoye21school <55892630+djoye21school@users.noreply.github.com>
2019-10-08 14:14:18 +0300

    new

--- saver.c
+++ saver.c
@@ -6,57 +6,61 @@
 /*   By: djoye <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/07 14:35:50 by djoye             #+#    #+#             */
-/*   Updated: 2019/10/07 19:18:06 by djoye            ###   ########.fr       */
+/*   Updated: 2019/10/08 14:13:35 by djoye            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
 
-#define	struct	s_point
+p_point		saver(char *str, p_point *p)
 {
-	char    name;
-	int     x[3];
-	int     y[3];
-}           p;
-
-int				saver(*str, *p)
-{
-	int 		i;
-	int			count;
-	int			pos;
-	int			pt;
+	int		i;
+	int		pos;
+	int		pt;
 
 	i = 0;
-	count = 0;
-	pt = 0;
-	while(str[i] != '\0')
+	pt = -1;
+	while (str[i] != '\0')
 	{
-		if(str[i] == '#' && count == 0)
+		if (str[i] == '#' && pt == -1)
 		{
 			pos = i;
-			count++;
+			pt++;
 		}
-		else if (str[i] == '#' && count != 0)
+		else if (str[i] == '#')
 		{
-			p->x[pt] = (i - pos - 5) % 5;
-			p->y[pt] = (i - pos) / 5 + (p->x[pt] < 0 ? 1 : 0);
+			p->x[pt] = i % 5 - pos % 5;
+			p->y[pt] = i / 5 - pos / 5;
 			pt++;
 		}
 		if (pt == 3)
-			return (p);
+			break ;
 		i++;
 	}
-	return (NULL);
+	return (*p);
 }
 
-int     main(int argc, char **argv)
+int			main(int argc, char **argv)
 {
-	int     fd;
-	char    buf[BUFF_SIZE + 1];
-	int     len;
+	int		i;
+	p_point	figures[26];
+	int		fd;
+	char	buf[BUFF_SIZE + 1];
+	int		len;
+	int		pt;
 
+	pt = 0;
 	fd = open(argv[argc - 1], O_RDONLY);
-	while((len = read(fd, buf, BUFF_SIZE)) > 0)
-		printf("%s\n", saver (buf, &p));
+	while ((len = read(fd, buf, BUFF_SIZE)) > 0 && pt < 26)
+	{
+		figures[pt] = saver(buf, &figures[pt]);
+		i = 0;
+		while (i < 3)
+		{
+			printf("%d %d\n", figures[pt].x[i], figures[pt].y[i]);
+			i++;
+		}
+		pt++;
+	}
 	return (0);
 }
