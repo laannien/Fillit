f52b84e42ed03f78ee815433cf89c6beb6e0671f
Natalia Sharova <laan_nien@mail.ru>
2019-10-08 14:50:43 +0300

    modified saver a bit

--- saver.c
+++ saver.c
@@ -3,16 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   saver.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: djoye <marvin@42.fr>                       +#+  +:+       +#+        */
+/*   By: uheirloo <uheirloo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/07 14:35:50 by djoye             #+#    #+#             */
-/*   Updated: 2019/10/08 14:13:35 by djoye            ###   ########.fr       */
+/*   Updated: 2019/10/08 14:49:19 by uheirloo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
 
-p_point		saver(char *str, p_point *p)
+t_tetra		saver(char *str, t_tetra *figure, int index)
 {
 	int		i;
 	int		pos;
@@ -29,21 +29,25 @@ p_point		saver(char *str, p_point *p)
 		}
 		else if (str[i] == '#')
 		{
-			p->x[pt] = i % 5 - pos % 5;
-			p->y[pt] = i / 5 - pos / 5;
+			figure->x[pt] = i % 5 - pos % 5;
+			figure->y[pt] = i / 5 - pos / 5;
 			pt++;
 		}
 		if (pt == 3)
+		{
+			figure->letter = index + 'A';
 			break ;
+		}
+		
 		i++;
 	}
-	return (*p);
+	return (*figure);
 }
 
 int			main(int argc, char **argv)
 {
 	int		i;
-	p_point	figures[26];
+	t_tetra	figures[26];
 	int		fd;
 	char	buf[BUFF_SIZE + 1];
 	int		len;
@@ -53,7 +57,7 @@ int			main(int argc, char **argv)
 	fd = open(argv[argc - 1], O_RDONLY);
 	while ((len = read(fd, buf, BUFF_SIZE)) > 0 && pt < 26)
 	{
-		figures[pt] = saver(buf, &figures[pt]);
+		figures[pt] = saver(buf, &figures[pt], pt);
 		i = 0;
 		while (i < 3)
 		{
