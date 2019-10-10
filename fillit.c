4aaca06b3f5c1b7588aae27f3c57acdc2f83c5c3
Diedra Joye <djoye@at-i7.21-school.ru>
2019-10-10 18:18:25 +0300

    01

--- fillit.c
+++ fillit.c
@@ -6,7 +6,7 @@
 /*   By: djoye <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/10 13:04:14 by djoye             #+#    #+#             */
-/*   Updated: 2019/10/10 16:07:08 by djoye            ###   ########.fr       */
+/*   Updated: 2019/10/10 18:17:16 by djoye            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,44 +14,16 @@
 
 t_map		fillit(int count_fig, int x, int y, t_tetra *p, t_map *map)
 {
-	int		ind_fig;
+	int		i;
 
-	ind_fig = 0;
-	if (x < map->size && y < map->size && (map->matrix[y][x] == '.' 
-			|| map->matrix[y][x] == 0
-																))
-	{
-		if ((x + p->x[0] >= 0 && y + p->y[0] >= 0 && x + p->x[0] < map->size &&
-					y + p->y[0] < map->size &&
-					(map->matrix[y + p->y[0]][x + p->x[0]] == '.' 
-					 || map->matrix[y + p->y[0]][x + p->x[0]] == 0
-					 )) &&
-				(x + p->x[1] >= 0 && y + p->y[1] >= 0 &&
-				 x + p->x[1] < map->size && y + p->y[1] < map->size &&
-				 (map->matrix[y + p->y[1]][x + p->x[1]] == '.'
-				  || map->matrix[y + p->y[1]][x + p->x[1]] == 0
-				  )) &&
-				(x + p->x[2] >= 0 && y + p->y[2] >= 0 &&
-				 x + p->x[2] < map->size && y + p->y[2] < map->size &&
-				 (map->matrix[y + p->y[2]][x + p->x[2]] == '.' 
-				  || map->matrix[y + p->y[2]][x + p->x[2]] == 0
-			)))
-		{
-			map->matrix[y][x] = p->letter;
-			map->matrix[y + p->y[0]][x + p->x[0]] = p->letter;
-			map->matrix[y + p->y[1]][x + p->x[1]] = p->letter;
-			map->matrix[y + p->y[2]][x + p->x[2]] = p->letter;
-		}
-		if (++ind_fig == count_fig)
-			return (*map);
-		else return (fillit (count_fig, 0, 0, &p[ind_fig], map));
-	}
-	if (x <= (map->size - 2))
-		return (fillit(count_fig, x + 1, y, &p[ind_fig], map));
-	else if (x == (map->size -1) && y <= (map->size - 2))
-		return (fillit(count_fig, 0, y + 1, &p[ind_fig], map));
-	else if (x == (map->size - 1) && y == (map->size - 1) && ind_fig > 0)
-		return (fillit(count_fig, 0, 0, &p[--ind_fig], map));
-	else return (*map);
-//	else return (*get_matrix(count_fig, p, map));
+	i = 0;
+	if (i < (count_fig) && map->matrix[y][x] == '.' && push_figure(x, y, &p[i], map))
+		return (fillit (count_fig, 0, 0, &p[++i], map));
+	else if (i < (count_fig) && x <= (map->size - 2) && map->matrix[y][x + 1] == '.')
+		return (fillit(count_fig, x + 1, y, &p[i], map));
+	else if (i < (count_fig) && x == (map->size - 1) && y <= (map->size - 2) && map->matrix [y + 1][0] == '.')
+		return (fillit(count_fig, 0, y + 1, &p[i], map));
+//	if (i != count_fig)
+//		get_matrix(count_fig, p, map);
+	return (*map);
 }
